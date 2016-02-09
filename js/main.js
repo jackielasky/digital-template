50540afa6763d2bbbcdd2d4064bade0c1c8c5dc5
jackielasky <jlasky2@gmu.edu>
2016-02-08 23:46:25 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 function preload() {
 
     game.load.image('sky', 'assets/colormap2.png');
-    game.load.image('ground', 'assets/airplane.png');
+    game.load.image('ground', 'assets/platform.png');
     game.load.image('star', 'assets/smallPin.png');
     game.load.image('ground2', 'assets/airplane.png');
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
